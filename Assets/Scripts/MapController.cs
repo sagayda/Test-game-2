ebe2c910ea2c016114d53fae2ca0a6456e270475
sagayda <sagayda27@gmail.com>
2023-08-02 21:06:43 +0300

    map zoom and codestyle update

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -1,23 +1,15 @@
 using Assets.Scripts;
 using Assets.Scripts.InGameScripts;
 using System.Collections.Generic;
-using System.Net.NetworkInformation;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class MapController : MonoBehaviour
 {
-    #region map settings
     [SerializeField] private float _cameraMoveSpeed = 50f;
     [SerializeField] private float _cameraZoomSpeed = 4000f;
     [SerializeField] private Vector2 _borders = new Vector2(2.5f, 5f);
-
-    private float _cameraMinFov;
-    private readonly float _cameraMaxFov = 52;
-
-    private const int LOCATION_CELL_SIZE = 16;
-    #endregion
 
     [SerializeField] private TextQuest _textQuest;
 
@@ -37,17 +29,19 @@ public class MapController : MonoBehaviour
     [SerializeField] private Grid _mapGrid;
     [SerializeField] private AnimatedTile _playerMarkTile;
 
-    private bool _mapEnabled = false;
-    private bool _mapLoaded = false;
-    private bool _settingsLoaded = false;
-    private bool _worldLoaded = false;
+    public bool IsMapEnabled { get; private set; } = false;
+    public bool IsMapLoaded { get; private set; } = false;
 
     private GameWorld _gameWorld;
 
     private int _scaleLevel;
     private int _maxScaleLevel;
-    private float _fovStepPerScaleLevel;
-    private int _cellsCountStepPerScaleLevel = 64;
+    private float _minToMaxResolutionRatio = 1f / 4f;
+
+    private float _cameraMinFov;
+    private float _cameraMaxFov;
+
+    private const int LOCATION_CELL_SIZE = 16;
 
     private GameObject[] _mapSprites;
 
@@ -62,18 +56,16 @@ public class MapController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.M))
         {
             if (_gameWorld == null)
-            { 
-                Load();
-                return;
-            }
+                if (!Load())
+                    return;
 
-            if (_mapEnabled)
+            if (IsMapEnabled)
                 DisableMap();
             else
                 EnableMap();
         }
 
-        if (_mapEnabled)
+        if (IsMapEnabled)
         {
             #region cameraMoving
             float horizontalMovement = Input.GetAxis("Horizontal");
@@ -90,7 +82,7 @@ public class MapController : MonoBehaviour
             else if (verticalMovement < 0 && _mapCamera.transform.localPosition.y + 50 <= _borders.y * -1)
                 verticalMovement = 0;
 
-            Vector3 movement = new Vector3(horizontalMovement, verticalMovement, 0) * _cameraMoveSpeed * Time.deltaTime;
+            Vector3 movement = _cameraMoveSpeed * Time.deltaTime * new Vector3(horizontalMovement, verticalMovement, 0);
             _mapCamera.transform.Translate(movement);
 
             float zoomAmount = Input.GetAxis("Mouse ScrollWheel") * _cameraZoomSpeed * Time.deltaTime;
@@ -121,10 +113,17 @@ public class MapController : MonoBehaviour
 
     private void EnableMap()
     {
-        if(!_mapLoaded)
+        if (!IsMapLoaded)
         {
-            Debug.Log("Map is not loaded!");
-            return;
+            Debug.Log("Map is not loaded! Trying to load...");
+
+            if (!Load())
+            {
+                Debug.Log("Failed to load map!");
+                return;
+            }
+
+            Debug.Log("Successfully loaded map!");
         }
 
         _mainCamera.gameObject.SetActive(false);
@@ -134,11 +133,24 @@ public class MapController : MonoBehaviour
         _mapCanvas.gameObject.SetActive(true);
 
         EventBus.WorldEvents.onPlayerPositionChanged += MovePlayerMark;
-        _mapEnabled = true;
+        IsMapEnabled = true;
     }
 
     private void DisableMap()
     {
+        if (!IsMapLoaded)
+        {
+            Debug.Log("Map is not loaded! Trying to load...");
+
+            if (!Load())
+            {
+                Debug.Log("Failed to load map!");
+                return;
+            }
+
+            Debug.Log("Successfully loaded map!");
+        }
+
         _mapCamera.gameObject.SetActive(false);
         _mapCanvas.gameObject.SetActive(false);
 
@@ -146,11 +158,116 @@ public class MapController : MonoBehaviour
         _mainCanvas.gameObject.SetActive(true);
 
         Assets.Scripts.EventBus.WorldEvents.onPlayerPositionChanged -= MovePlayerMark;
-        _mapEnabled = false;
+        IsMapEnabled = false;
     }
 
-    private Texture2D PaintMap(Texture2D texture, GameWorld world, int width, int height)
+
+    private bool LoadWorld()
     {
+        GameWorld gameWorld = _textQuest.GetGameWorld();
+
+        if (gameWorld == null)
+            return false;
+
+        _gameWorld = gameWorld;
+
+        return true;
+    }
+
+    private bool LoadMap()
+    {
+        _mapSprites = new GameObject[_maxScaleLevel + 1];
+
+        for (int scaleLevel = 0; scaleLevel < _mapSprites.Length; scaleLevel++)
+        {
+            if (scaleLevel == _maxScaleLevel)
+            {
+                Debug.Log("Max scale is not implemented");
+                continue;
+            }
+
+            int mapWidth = Mathf.RoundToInt(_gameWorld.WorldWidth * ((_maxScaleLevel - scaleLevel) * _minToMaxResolutionRatio));
+            int mapHeight = Mathf.RoundToInt(_gameWorld.WorldHeight * ((_maxScaleLevel - scaleLevel) * _minToMaxResolutionRatio));
+
+            Texture2D texture = PaintMap(_gameWorld, mapWidth, mapHeight);
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
+
+            GameObject mapObject = Instantiate(_mapSpriteObjectPrefab, _mapParentTransform);
+
+            int largerEdge = texture.width >= texture.height ? texture.width : texture.height;
+
+            mapObject.transform.localScale = new Vector2(100f / largerEdge * 100f, 100f / largerEdge * 100f);
+            mapObject.GetComponent<SpriteRenderer>().sprite = sprite;
+            mapObject.SetActive(false);
+            mapObject.name = $"Map texture {_maxScaleLevel - scaleLevel - 1} [{texture.width}x{texture.height}]";
+
+            _mapSprites[_maxScaleLevel - scaleLevel - 1] = mapObject;
+        }
+
+        SetMapTexture(0);
+
+        return true;
+    }
+
+    private bool LoadSettings()
+    {
+        _mapGrid.cellSize = new Vector3(100f / _gameWorld.WorldWidth, 100f / _gameWorld.WorldHeight);
+
+        int worldSize = _gameWorld.WorldWidth > _gameWorld.WorldHeight ? _gameWorld.WorldWidth : _gameWorld.WorldHeight;
+
+        _mapCamera.farClipPlane = worldSize * 2;
+        _cameraMinFov = 200f / worldSize;
+        _cameraMaxFov = _gameWorld.WorldWidth > _gameWorld.WorldHeight ? 34f : 61f;
+        _mapCamera.fieldOfView = _cameraMaxFov;
+
+        _maxScaleLevel = Mathf.CeilToInt(1f / _minToMaxResolutionRatio);
+        _scaleLevel = 0;
+
+        return true;
+    }
+
+    private bool Load()
+    {
+        if (!LoadWorld())
+        {
+            Debug.Log("Failed to load world!");
+            return false;
+        }
+
+        if (!LoadSettings())
+        {
+            Debug.Log("Failed to load settings!");
+            return false;
+        }
+
+        if (!LoadMap())
+        {
+            Debug.Log("Failed to load map!");
+            return false;
+        }
+
+        IsMapLoaded = true;
+
+        return true;
+    }
+
+    private void SetMapTexture(int scaleLevel)
+    {
+        foreach (var map in _mapSprites)
+        {
+            if (map == null)
+                continue;
+
+            map.SetActive(false);
+        }
+
+        _mapSprites[scaleLevel].SetActive(true);
+    }
+
+    private Texture2D PaintMap(GameWorld world, int width, int height)
+    {
+        Texture2D texture = new(width * LOCATION_CELL_SIZE, height * LOCATION_CELL_SIZE);
+
         float horizontalRatio = world.WorldWidth / (float)width;
         float verticalRatio = world.WorldHeight / (float)height;
 
@@ -184,18 +301,18 @@ public class MapController : MonoBehaviour
 
         Color GetColorInSquare(GameWorld world, int xStart, int yStart, int xFinish, int yFinish)
         {
-            Dictionary<Color,float> colorCounts = new Dictionary<Color,float>();
+            Dictionary<Color, float> colorCounts = new Dictionary<Color, float>();
 
             for (int i = xStart; i <= xFinish; i++)
             {
                 for (int j = yStart; j <= yFinish; j++)
                 {
-                    Color locationColor = world.World[i, j] == null ? Color.white : world.World[i,j].Color;
+                    Color locationColor = world.World[i, j] == null ? Color.white : world.World[i, j].Color;
 
                     float increment = 1f;
 
                     if (world.World[i, j].Id == 3)
-                        increment = 1.65f;
+                        increment = 3.5f;
 
                     if (colorCounts.ContainsKey(locationColor))
                     {
@@ -214,7 +331,7 @@ public class MapController : MonoBehaviour
 
             foreach (var pair in colorCounts)
             {
-                if(pair.Value > maxCount)
+                if (pair.Value > maxCount)
                 {
                     resultColor = pair.Key;
                     maxCount = pair.Value;
@@ -238,157 +355,24 @@ public class MapController : MonoBehaviour
 
     }
 
-    private bool LoadWorld()
-    {
-        GameWorld gameWorld = _textQuest.GetGameWorld();
-
-        if (gameWorld == null)
-            return false;
-
-        _gameWorld = gameWorld;
-        _worldLoaded = true;
-        return true;
-    }
-
-    private bool LoadMap()
-    {
-        if (!_worldLoaded || !_settingsLoaded)
-            return false;
-
-        _mapSprites = new GameObject[_maxScaleLevel + 1];
-
-        //Debug.Log($"{_gameWorld.WorldWidth}, {_gameWorld.WorldHeight}");
-
-        //_mapTextures[0] = PaintMap(new Texture2D(_gameWorld.WorldWidth * LOCATION_CELL_SIZE, _gameWorld.WorldHeight * LOCATION_CELL_SIZE), _gameWorld, _gameWorld.WorldWidth, _gameWorld.WorldHeight);
-
-        //for (int i = 1; i < _mapTextures.Length; i++)
-        //{
-        //    _mapTextures[i] = _mapTextures[0];
-        //}
-
-        for (int i = 0; i < _mapSprites.Length; i++)
-        {
-            if (i == _maxScaleLevel)
-                continue;
-
-            //int j = i;
-            int j = _maxScaleLevel - i;
-
-            int textureSize = j * _cellsCountStepPerScaleLevel * LOCATION_CELL_SIZE;
-
-            Debug.Log($" i : {i}, txtr size : {textureSize}, width : {j * _cellsCountStepPerScaleLevel}");
-
-            Texture2D texture = PaintMap(new Texture2D(textureSize, textureSize), _gameWorld, j * _cellsCountStepPerScaleLevel, j * _cellsCountStepPerScaleLevel);
-            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
-
-            GameObject mapObject = Instantiate(_mapSpriteObjectPrefab, _mapParentTransform);
-            mapObject.transform.localScale = new Vector2(100f / texture.width * 100f, 100f / texture.height * 100f);
-            mapObject.GetComponent<SpriteRenderer>().sprite = sprite;
-            mapObject.SetActive(false);
-            mapObject.name = $"Map texture {_maxScaleLevel - i - 1} [{texture.width}x{texture.height}]";
-
-            _mapSprites[_maxScaleLevel - i - 1] = mapObject;
-
-            //_mapTextures[_maxScaleLevel - i - 1] = PaintMap(new Texture2D(textureSize, textureSize), _gameWorld, j * _cellsCountStepPerScaleLevel, j * _cellsCountStepPerScaleLevel);
-
-
-        }
-
-        SetMapTexture(1);
-
-        //Texture2D worldTexture = new(worldSize * LOCATION_CELL_SIZE, worldSize * LOCATION_CELL_SIZE);
-
-        //PaintMap(worldTexture);
-
-        //worldTexture.filterMode = FilterMode.Point;
-        //worldTexture.Apply();
-
-
-        //Sprite sprite = Sprite.Create(worldTexture, new Rect(0, 0, worldSize * LOCATION_CELL_SIZE, worldSize * LOCATION_CELL_SIZE), new Vector2(0, 0));
-
-        //_mapObject.transform.localScale = new Vector2((100f / (worldSize * LOCATION_CELL_SIZE)) * 100f, (100f / (worldSize * LOCATION_CELL_SIZE)) * 100f);
-        //_mapObject.GetComponent<SpriteRenderer>().sprite = sprite;
-
-        //_mapGrid.GetComponentInChildren<Tilemap>().SetTile(new Vector3Int(Mathf.RoundToInt(_gameWorld.Players[0].VectorPosition.x), Mathf.RoundToInt(_gameWorld.Players[0].VectorPosition.y) + 2, 0), _playerMarkTile);
-
-        _mapLoaded = true;
-        return true;
-    }
-
-    private bool LoadSettings()
-    {
-        if(!_worldLoaded)
-            return false;
-
-        _mapGrid.cellSize = new Vector3(100f / _gameWorld.WorldWidth, 100f / _gameWorld.WorldHeight);
-
-        int worldSize = _gameWorld.WorldWidth > _gameWorld.WorldHeight ? _gameWorld.WorldWidth : _gameWorld.WorldHeight;
-
-        _mapCamera.farClipPlane = worldSize * 2;
-        _cameraMinFov = 200f / worldSize;
-        _mapCamera.fieldOfView = _cameraMaxFov;
-
-        _cellsCountStepPerScaleLevel = worldSize / 4;
-
-        _maxScaleLevel = Mathf.CeilToInt(worldSize / _cellsCountStepPerScaleLevel);
-        _scaleLevel = 0;
-        _fovStepPerScaleLevel= (_cameraMaxFov - _cameraMinFov) / _maxScaleLevel;
-
-        _settingsLoaded = true;
-        return true;
-    }
-
-    private bool Load()
-    {
-        if (!LoadWorld())
-        {
-            Debug.Log("Failed to load world!");
-            return false;
-        }
-
-        if (!LoadSettings())
-        {
-            Debug.Log("Failed to load settings!");
-            return false;
-        }
-
-        if (!LoadMap()) 
-        {
-            Debug.Log("Failed to load map!");
-            return false;
-        }
-
-        return true;
-    }
-
-    private void SetMapTexture(int scaleLevel)
-    {
-        Debug.Log($"Setting texture {scaleLevel}");
-
-        foreach (var map in _mapSprites)
-        {
-            if(map == null)
-                continue;
-
-            map.SetActive(false);
-        }
-
-        _mapSprites[scaleLevel].SetActive(true);
-    }
-
     private void CheckScale()
     {
-        if ((_maxScaleLevel - (_scaleLevel +1)) * _fovStepPerScaleLevel + _cameraMinFov > _mapCamera.fieldOfView && _scaleLevel != _maxScaleLevel)
+        float fovStepPerScaleLevel = (_cameraMaxFov - _cameraMinFov) / _maxScaleLevel;
+
+        float lowerBound = (_maxScaleLevel - (_scaleLevel + 1)) * fovStepPerScaleLevel + _cameraMinFov;
+        float upperBound = (_maxScaleLevel - (_scaleLevel + 1) + 1) * fovStepPerScaleLevel + _cameraMinFov;
+
+        if (_mapCamera.fieldOfView < lowerBound && _scaleLevel != _maxScaleLevel)
         {
             IncreaseScaleLevel();
-            Debug.Log($"Upscale: sc lvl: {_scaleLevel}, ({_maxScaleLevel} - {_scaleLevel}) * {_fovStepPerScaleLevel} + {_cameraMinFov} > {_mapCamera.fieldOfView}");
+            Debug.Log($"Upscale: sc lvl: {_scaleLevel}, ({_maxScaleLevel} - {_scaleLevel}) * {fovStepPerScaleLevel} + {_cameraMinFov} > {_mapCamera.fieldOfView}");
             return;
         }
 
-        if ((_maxScaleLevel - (_scaleLevel +1) + 1) * _fovStepPerScaleLevel + _cameraMinFov < _mapCamera.fieldOfView && _scaleLevel != 0)
+        if (_mapCamera.fieldOfView > upperBound && _scaleLevel != 0)
         {
             DecreaseScaleLevel();
-            Debug.Log($"Downscale: sc lvl: {_scaleLevel}, ({_maxScaleLevel} - {_scaleLevel} + 1) * {_fovStepPerScaleLevel} + {_cameraMinFov} < {_mapCamera.fieldOfView}");
+            Debug.Log($"Downscale: sc lvl: {_scaleLevel}, ({_maxScaleLevel} - {_scaleLevel} + 1) * {fovStepPerScaleLevel} + {_cameraMinFov} < {_mapCamera.fieldOfView}");
             return;
         }
     }
@@ -401,12 +385,15 @@ public class MapController : MonoBehaviour
         }
 
         _scaleLevel++;
+
+        Texture2D texture = _mapSprites[_scaleLevel].GetComponent<SpriteRenderer>().sprite.texture;
+        int textureSize = texture.width >= texture.height ? texture.width : texture.height;
+
         Vector2 gridCellSize = new()
         {
-            x = 100f / (_mapSprites[_scaleLevel].GetComponent<SpriteRenderer>().sprite.texture.width / 16f),
-            y = 100f / (_mapSprites[_scaleLevel].GetComponent<SpriteRenderer>().sprite.texture.height / 16f),
+            x = 100f / (textureSize / 16f),
+            y = 100f / (textureSize / 16f),
         };
-        //_mapGrid.cellSize = new Vector3(100f / (_mapSprites[_scaleLevel].GetComponent<SpriteRenderer>().sprite.texture.width / 16f), 100f / (_mapSprites[_scaleLevel].GetComponent<SpriteRenderer>().sprite.texture.height / 16f));
 
         _mapGrid.cellSize = gridCellSize;
 
@@ -423,12 +410,14 @@ public class MapController : MonoBehaviour
 
         _scaleLevel--;
 
+        Texture2D texture = _mapSprites[_scaleLevel].GetComponent<SpriteRenderer>().sprite.texture;
+        int textureSize = texture.width >= texture.height ? texture.width : texture.height;
+
         Vector2 gridCellSize = new()
         {
-            x = 100f / (_mapSprites[_scaleLevel].GetComponent<SpriteRenderer>().sprite.texture.width / 16f),
-            y = 100f / (_mapSprites[_scaleLevel].GetComponent<SpriteRenderer>().sprite.texture.height / 16f),
+            x = 100f / (textureSize / 16f),
+            y = 100f / (textureSize / 16f),
         };
-        //_mapGrid.cellSize = new Vector3(100f / (_mapSprites[_scaleLevel].GetComponent<SpriteRenderer>().sprite.texture.width / 16f), 100f / (_mapSprites[_scaleLevel].GetComponent<SpriteRenderer>().sprite.texture.height / 16f));
 
         _mapGrid.cellSize = gridCellSize;
 
@@ -459,10 +448,11 @@ public class MapController : MonoBehaviour
 
     private Vector2 WorldCoordinatesToGame(Vector2 worldCoords)
     {
-        Vector2 gameCoords = new();
-
-        gameCoords.x = math.floor(worldCoords.x / (100f / _gameWorld.WorldWidth));
-        gameCoords.y = math.floor((worldCoords.y * -1) / (100f / _gameWorld.WorldWidth));
+        Vector2 gameCoords = new()
+        {
+            x = math.floor(worldCoords.x / (100f / _gameWorld.WorldWidth)),
+            y = math.floor((worldCoords.y * -1) / (100f / _gameWorld.WorldWidth))
+        };
 
         return gameCoords;
     }
